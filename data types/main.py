d41af05a27c1e150a6cf506cb5c3c7b0f274d51f
jiya-waqas <jiyawaqas1708@gmail.com>
2025-09-18 22:59:20 +0500

    number swaping

---
+++ data types/main.py
@@ -0,0 +1,17 @@
+name = "josh"
+age= 34
+wieght=74.3
+isStudent= True
+
+print('value',name,"data type",type(name))
+print('value',age,"data type",type(age))
+
+age =float(age)
+print("converted to float", age)
+
+weight =int(weight)
+print("coverted to int :", weight)
+
+num="100"
+num=int(num)
+print("value",num," j ")
---
+++ operators/main.py
@@ -0,0 +1,11 @@
+num1= int(input("enter a value for a:"))
+print("input a value:",num1)
+
+num2= int(input("enter a 2nd value:"))
+print("enter value num2:",num2)
+
+print("addition:",num1+num2) 
+print("subtraction:",num1-num2)
+print("divide:",num1/num2)
+print("multiply:",num1*num2)
+print("addition:",num1+num2)
